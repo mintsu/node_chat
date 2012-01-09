99404c2a582fe52c89171f234c46e776e9745ef3
mintsu <mintsu7531@gmail.com>
2012-01-10 02:50:06 +0900

    changed to server.js and client.js

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,5 @@
-var socket = io.connect('http://mintsu1.nodester.com');
-
+var socket = io.connect();
+socket.on('connect',function(){
 	socket.on('a', function(data){
 		console.log("setsuzoku");
 		$('#count').text(data);
@@ -8,6 +8,7 @@ var socket = io.connect('http://mintsu1.nodester.com');
 		$('#chat').append('<p>' + escape(msg.name) + ':' + escape(msg.text) + '</p><hr />');
 		$('#chat').children('p:last').hide().fadeIn(5000);
 	});
+});
 
 function send()
 {
--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.listen(13804);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 var io = require('socket.io').listen(app);
+io.set('transports', ['xhr-polling']);
 var count=0;
 io.sockets.on('connection', function(socket){
 	count++;
