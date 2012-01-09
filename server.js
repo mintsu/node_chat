7530b39cdb066ed2efa40cbb163fe67c3812e144
mintsu <mintsu7531@gmail.com>
2012-01-10 00:32:46 +0900

    server.js changed

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', function(req,res){
 	});		
 });
 
-app.listen(13801);
+app.listen(13804);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 var io = require('socket.io').listen(app);
