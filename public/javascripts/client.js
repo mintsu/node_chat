df7ce1bbcb14f40b1f2bc036cd61912b6b358bc0
mintsu <mintsu7531@gmail.com>
2012-01-10 00:21:04 +0900

    first commit

---
+++ public/javascripts/client.js
@@ -0,0 +1,26 @@
+var socket = io.connect();
+socket.on('connect',function(){
+	socket.on('a', function(data){
+		$('#count').text(data);
+	});
+	socket.on('msg', function(msg){
+		$('#chat').append('<p>' + escape(msg.name) + ':' + escape(msg.text) + '</p><hr />');
+		$('#chat').children('p:last').hide().fadeIn(5000);
+	});
+});
+
+function send()
+{
+	var name = $('#name').val();
+	var text = $('#text').val();
+	if(text && name){
+		socket.emit('msg', {name: name, text: text});
+		$('#text').val('');
+	}
+}
+
+function escape(str){
+	return $('<div></div>').text(str).html();
+}
+
+
